efffa3f2ed34119ebef536fe16b20c5654f843da
Vitaliy <pentagon0555@gmail.com>
2017-07-29 23:57:02 +0300

    task1

---
+++ task1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(averageTwoNums(a, b));
+    }
+
+    static double averageTwoNums(double a, double b) {
+        return (a + b) / 2;
+    }
+}
